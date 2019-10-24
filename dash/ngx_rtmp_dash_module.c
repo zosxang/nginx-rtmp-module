5e47cde1e040ed912bd657e3156388a2214632ee
Chetan Dhembre <chetandhembre@gmail.com>
2019-10-24 13:13:42 +0530

    added concat

--- dash/ngx_rtmp_dash_module.c
+++ dash/ngx_rtmp_dash_module.c
@@ -384,7 +384,7 @@ static ngx_int_t
 ngx_rtmp_dash_rename_file(u_char *src, u_char *dst)
 {
     /* rename file with overwrite */
-
+ 
 #if (NGX_WIN32)
     return MoveFileEx((LPCTSTR) src, (LPCTSTR) dst, MOVEFILE_REPLACE_EXISTING);
 #else
@@ -496,6 +496,15 @@ ngx_rtmp_dash_write_content_protection(ngx_rtmp_session_t *s,
     return p;
 }
 
+u_char* concat(u_char *src1, u_char *src2, int start_index)
+{
+  for(int i = 0; i < sizeof(src2); i++)
+    {
+      src1[start_index + i] = src2[i];
+      
+    }
+    return src1;
+}
 
 static ngx_int_t
 ngx_rtmp_dash_write_variant_playlist(ngx_rtmp_session_t *s)
@@ -526,6 +535,10 @@ ngx_rtmp_dash_write_variant_playlist(ngx_rtmp_session_t *s)
     static u_char              buffer_depth[sizeof("P00Y00M00DT00H00M00.000S")];
     static u_char              frame_rate[(NGX_INT_T_LEN * 2) + 2];
     static u_char              seg_path[NGX_MAX_PATH + 1];
+
+    static int                TOTAL_BUFFER_PARTS = 10;
+    static u_char             buffr[NGX_RTMP_DASH_BUFSIZE * TOTAL_BUFFER_PARTS];
+    int                       buffr_index =0;
 
     dacf = ngx_rtmp_get_module_app_conf(s, ngx_rtmp_dash_module);
     ctx = ngx_rtmp_get_module_ctx(s, ngx_rtmp_dash_module);
@@ -642,9 +655,11 @@ ngx_rtmp_dash_write_variant_playlist(ngx_rtmp_session_t *s)
                      );
 
     p = ngx_slprintf(p, last, NGX_RTMP_DASH_MANIFEST_PERIOD);
-
+    
     n = ngx_write_fd(fd, buffer, p - buffer);
-
+    concat(buffr, buffer)
+    buffr_index = buffr_index + sizeof(buffer)
+    ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "buffr '%s' %i",  buffr, buffr_index);
     sep = (dacf->nested ? "/" : "-");
     var = dacf->variant->elts;
 
@@ -815,7 +830,7 @@ ngx_rtmp_dash_write_variant_playlist(ngx_rtmp_session_t *s)
     }
 
     ngx_close_file(fd);
-
+    ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "renaming file '%s':'%s'", src, dst);
     if (ngx_rtmp_dash_rename_file(ctx->var_playlist_bak.data, ctx->var_playlist.data)
         == NGX_FILE_ERROR)
     {
